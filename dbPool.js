3a67773529585674c656562f3bd87085580ac399
sungwoo1548 <swo1548@gmail.com>
2019-12-26 23:07:31 +0900

    modi heroku setting

--- dbPool.js
+++ dbPool.js
@@ -1,11 +1,7 @@
 const mysql = require("mysql");
 
-const dbconfig = process.env.CLEARDB_DATABASE_URL || {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "photo_db",
-};
+const dbconfig = process.env.CLEARDB_DATABASE_URL && { host: "us-cdbr-iron-east-05.cleardb.net", user: "b6cae4aeb83344", password: "93fd146c", database: "heroku_939679a53fddc87" }
+    || { host: "us-cdbr-iron-east-05.cleardb.net", user: "b6cae4aeb83344", password: "93fd146c", database: "heroku_939679a53fddc87" };
 
 const dbPool = mysql.createPool({
     connectionLimit: 10,
