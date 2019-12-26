25ba690525c928246a5c9a0c99d0a69a51ec5ec8
sungwoo1548 <swo1548@gmail.com>
2019-12-27 00:52:52 +0900

    fix typo

--- dbPool.js
+++ dbPool.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
 
-const dbconfig = process.env.CLEARDB_DATABASE_URL && { host: "us-cdbr-iron-east-05.cleardb.net", user: "b6cae4aeb83344", password: "93fd146c", database: "heroku_939679a53fddc87" }
-    || { host: "us-cdbr-iron-east-05.cleardb.net", user: "b6cae4aeb83344", password: "93fd146c", database: "heroku_939679a53fddc87" };
+const dbconfig = process.env.CLEARDB_DATABASE_URL
+    ? { host: "us-cdbr-iron-east-05.cleardb.net", user: "b6cae4aeb83344", password: "93fd146c", database: "heroku_939679a53fddc87" }
+    : { host: "localhost", user: "root", password: "root", database: "photo_db" };
 
 const dbPool = mysql.createPool({
     connectionLimit: 10,
