c32ea26450f80f32796f8ab095a8c5b6e5b4f5a7
sungwoo1548 <swo1548@gmail.com>
2019-12-26 21:19:27 +0900

    set middle ware & dbconnection

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const mysql = require("mysql");
 
 const app = express();
 
-app.use("/home", require("./routers/home"));
+// DB creatConnection
+const dbconfig = process.env.CLEARDB_DATABASE_URL || {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "photo_db"
+};
+const DBconnection = mysql.createConnection(dbconfig);
+
+
+app.use((req, res, next) => {
+    DBconnection.connect((err) => {
+        if (err) console.error(err);
+        else {
+            console.log("DB connect!!");
+            next();
+        };
+    })
+});
+
+app.use("/", require("./routers/home"));
+
+app.use(() => DBconnection.end((err) => {
+    if (err) console.error(err);
+    else console.log("DB disconnect//")
+}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
--- routers/home.js
+++ routers/home.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
     res.json({ msg: "connection test" });
-})
+    next();
+});
 
 module.exports = router;
