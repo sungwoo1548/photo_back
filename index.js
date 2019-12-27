800d554aa73380d2c66f07c220c33cabbc9cf891
swo1548 <swo1548@gmail.com>
2019-12-27 09:35:37 +0900

    test log

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/", require("./routers/home"));
-app.use("/user", require("./routers/user"));
+app.use("/user", require("./routers/user")); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
